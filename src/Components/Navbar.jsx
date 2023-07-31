f9c5600098fd4407bad4867080262f1c978d2cca
Kestas <kestutisjankevicius9@gmail.com>
2023-07-31 21:05:53 +0300

    Navbutton

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom';
-import '../Style/Navbar';
-
+import '../Style/Navbar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 
 function Navbar() {
+
+    const [showNavBar, setNavBar] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setNavBar(false)
+    },[location])
+
+
   return (
-    <div className='navbar'>
+    <div className='navbar' id={showNavBar ? 'open' : 'close'}>
         <div className='navbutton'>
-            <button></button>
+            <button onClick={() => {setNavBar((prev) => !prev)}}><ReorderIcon/></button>
+        </div>
+        <div className='links'>
+            <Link to={'/'}> Home </Link>
+            <Link to={'/projects'}> Projects </Link>
+            <Link to={'/experience'}> Experience </Link>
         </div>
     </div>
 
