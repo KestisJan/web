63fbf38da5aed124e27dbcdea6bd519cd1518f97
Kestas <kestutisjankevicius9@gmail.com>
2023-07-31 20:39:15 +0300

    Beginning
    Just casual coding, getting trying to learn react better.

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link, useLocation } from 'react-router-dom';
+import '../Style/Navbar';
+
+
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='navbutton'>
+            <button></button>
+        </div>
+    </div>
+
+  )
+}
+
+export default Navbar
